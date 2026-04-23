7d2239e9ba3d53bb08ebd1278c4c75a4e3318c6c
esenecal <55216700+esenecal@users.noreply.github.com>
2026-04-23 14:17:15 -0600

    S1: created basic countdown timer using setInterval.

--- RESOURCES.md
+++ RESOURCES.md
@@ -1,3 +1,6 @@
 # Resources
 
 Various resources/references that were helpful in creating this project.
+
+[W3 Schools JS Timing](https://www.w3schools.com/js/js_timing.asp): Article on use of `setTimeout` and `setInterval`.
+[GeeksForGeeks JSdoc Comments](https://www.geeksforgeeks.org/javascript/documentation-comments-in-jsdoc/): use of JSdoc comments.
--- timer.js
+++ timer.js
@@ -1,9 +1,24 @@
 /**
- * Execute a function after a set amount of time.
+ * Displays a timer completion message after a set amount of time.
  * @param {number} time The amount of time the timer counts down from.
  */
 function timer(time) {      // timer function with a set 
     setTimeout(() => {console.log("timer over")}, time);
+}
+
+/**
+ * Counts down from a set amount of time and displays a completion message.
+ * @param {number} time The amount of time the timer counts down from in seconds
+ */
+function countdown(time) {
+    var count = time;                           // count keeps track of the countdown.
+    var myInterval = setInterval(() => {        // every 1 second...
+        count--;                                // ...decrement count...
+        console.log(count);                     // ...display count...
+        if (count == 0) {                       // ...if count is 0, stop the interval.
+            clearInterval(myInterval);
+        }
+    }, 1000);
 }
 
 /**
@@ -15,4 +30,6 @@ function secondsToMilli(seconds) {
     return seconds * 1000;
 }
 
-timer(secondsToMilli(10));
+console.log("START");
+// timer(secondsToMilli(10));
+countdown(5);
