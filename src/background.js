cdf530514872dd1e8d7601ed51fe224ce726d91d
esenecal <55216700+esenecal@users.noreply.github.com>
2026-04-24 14:10:31 -0600

    S2: retrieves the time value from time-input and passes it to the backend.

--- src/background.js
+++ src/background.js
@@ -1,5 +1,9 @@
 // background.js
 
+/**
+ * Indicate the start and finish of a timeout in the console.
+ * @param {number} time Time to countdown from in milliseconds
+ */
 function timer(time) {
     console.log("start");
     setTimeout(() => {
@@ -7,10 +11,19 @@ function timer(time) {
     }, time);
 }
 
+/**
+ * Convert seconds to milliseconds.
+ * @param {number} seconds The amount of time in seconds to be converted to ms.
+ * @returns {number} The original time in milliseconds. 
+ */
+function secondsToMilli(seconds) {
+    return seconds * 1000;
+}
+
 // receives the message from popup.js. Uses request to check which action was sent. Responds and prints to the console.
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'start') {
-        timer(5000);
+        timer(secondsToMilli(request.time));    // get the time from the time-input in the DOM, sent in the request.
         sendResponse({ logged: true });
     }
 });
--- src/popup.js
+++ src/popup.js
@@ -49,14 +49,17 @@ function secondsToMilli(seconds) {
  * Start the timer by sending a message to background.js
  */
 async function startTimer() {
+    var timeInput = document.getElementById("time-input");                      // get input element from DOM.
+
     // sends a message to the background script, as defined in manifest.json. Sends a message with an action "start"
     var response = await browser.runtime.sendMessage({
-        action: 'start'
+        action: 'start',
+        time: timeInput.value           // send the time input in the object.
     });
     console.log(response);
 }
 
-// Event Handlers
+// Event Handlers. Onclick at the elements defined by the ID, the associated function is executed.
 document.getElementById("timer").addEventListener('click', startTimer);
 document.getElementById("countdown-timer").addEventListener('click', countdown);
 
